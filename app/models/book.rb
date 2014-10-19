1b43936387ba4df24f1d3c35be72db7029e6d1ae
kyp <kyp108@gmail.com>
2014-10-19 18:13:57 +0200

    added validations to book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,5 @@
 class Book < ActiveRecord::Base
+	validates :title, presence: true
+	validates :author, presence: true
+	validates :rating, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10}
 end
